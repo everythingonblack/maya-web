e253760512ffc5cff298ae9025a0e0d0735ac844
jaya <jaya@gmail.com>
2025-11-14 06:13:26 +0700

    ok

--- apps/isomorphic/src/app/(hydrogen)/maya-cs/page.tsx
+++ apps/isomorphic/src/app/(hydrogen)/maya-cs/page.tsx
@@ -3,6 +3,9 @@ import PledgesNumber from '@/app/shared/appointment/dashboard/appointment-todo';
 import WelcomeBanner from './welcome-banner';
 import PlatformAccess from './platform-access';
 import FleetStatus from './fleet-status';
+import ResponseRate from '@/app/(hydrogen)/maya-cs/response-rate';
+import SalesReport from '@/app/(hydrogen)/maya-cs/sales-report';
+import RevenueExpense from '@/app/(hydrogen)/maya-cs/revenue-expense';
 
 export default function BiddingDashboard() {
     return (
@@ -12,6 +15,9 @@ export default function BiddingDashboard() {
                 <div className="grid grid-cols-1 @3xl:grid-cols-2 gap-4">
                     <PlatformAccess className="w-full" />
                     <FleetStatus className="w-full" />
+                    <SalesReport className="@4xl:col-span-2 @7xl:col-span-7" />
+                    <RevenueExpense className="@3xl/jd:col-span-full @5xl:col-span-7" />
+                    <ResponseRate className="col-span-full @4xl:col-span-6 @6xl:col-span-7" />   
                 </div>
 
             </div>
